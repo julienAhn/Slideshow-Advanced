3fc8ccebc740f5f4d0be2c39bea1a035e8fc784a
Julien Ahn <julien.ahn@skynet.be>
2023-03-30 22:47:53 +0200

    add ChatGPT code

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,208 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_slide_show/image_slide_show.dart';
+
+/*
+Explanation:
+
+- The app has a single screen (`HomePage`) with a button to select folders and an `ImageSlideShow` widget that displays the images in the selected folders.
+- When the user clicks the "Select Folders" button, a dialog (`FolderPickerDialog`) is shown that allows the user to navigate through the file system and select one or more folders. The `FolderPickerDialog` uses the `FolderPicker` package to display a folder picker interface.
+- When the user selects one or more folders in the `FolderPickerDialog`, the selected image paths are retrieved using the `_getImagePaths` method and stored in the `_imagePaths` list.
+- The `_getImagePaths` method recursively scans the folder for images with the `.jpg` or `.png` extension and returns their file paths.
+- The `ImageSlideShow` widget is used to display the images in a slideshow format. The `imageUrls` property is set to the `_imagePaths` list, and the `interval`, `isAutoPlay`, and `isLoop` properties are set to control the slideshow behavior.
+- The app is designed to support landscape mode only, and the `SystemChrome.setEnabledSystemUIOverlays` and `SystemChrome.setPreferredOrientations` methods are used to ensure that the app is displayed in landscape mode and with the system UI hidden during folder selection.
+*/
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Slideshow',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  List<String> _imagePaths = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _selectFolders() async {
+    final List<Directory>? selectedDirectories = await showDialog<List<Directory>>(
+      context: context,
+      builder: (BuildContext context) {
+        return FolderPickerDialog();
+      },
+    );
+    if (selectedDirectories != null) {
+      List<String> allPaths = [];
+      for (Directory dir in selectedDirectories) {
+        allPaths.addAll(await _getImagePaths(dir.path));
+      }
+      setState(() {
+        _imagePaths = allPaths;
+      });
+    }
+  }
+
+  Future<List<String>> _getImagePaths(String folderPath) async {
+    List<String> imagePaths = [];
+    Directory folder = Directory(folderPath);
+    List<FileSystemEntity> entities = await folder.list().toList();
+    for (FileSystemEntity entity in entities) {
+      if (entity is File) {
+        String path = entity.path;
+        if (path.endsWith('.jpg') || path.endsWith('.png')) {
+          imagePaths.add(path);
+        }
+      }
+    }
+    return imagePaths;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Slideshow'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              onPressed: _selectFolders,
+              child: Text('Select Folders'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 20),
+            _imagePaths.isEmpty
+                ? Text('No Images Selected')
+                : Expanded(
+              child: ImageSlideShow(
+                imageUrls: _imagePaths,
+                interval: Duration(seconds: 3),
+                isAutoPlay: true,
+                isLoop: true,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class FolderPickerDialog extends StatefulWidget {
+  @override
+  _FolderPickerDialogState createState() => _FolderPickerDialogState();
+}
+
+class _FolderPickerDialogState extends State<FolderPickerDialog> {
+  late Directory _rootDirectory;
+  late Directory _currentDirectory;
+  late List<Directory> _selectedDirectories;
+
+  @override
+  void initState() {
+    super.initState();
+    _rootDirectory = Directory('/');
+    _currentDirectory = _rootDirectory;
+    _selectedDirectories = [];
+  }
+
+  Future<void> _selectDirectory(Directory directory) async {
+    setState(() {
+      _currentDirectory = directory;
+    });
+  }
+
+  Future<void> _toggleDirectorySelection(Directory directory) async {
+    setState(() {
+      if (_selectedDirectories.contains(directory)) {
+        _selectedDirectories.remove(directory);
+      } else {
+        _selectedDirectories.add(directory);
+      }
+    });
+  }
+
+  Future<void> _navigateUp() async {
+    String parentPath = _currentDirectory.parent.path;
+    Directory parentDirectory = Directory(parentPath);
+    await _selectDirectory(parentDirectory);
+  }
+
+  Future<List<Directory>> _selectFolders() async {
+    await SystemChrome.setEnabledSystemUIOverlays([]);
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft,]);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select Folders'),
+          content: Container(
+            width: double.maxFinite,
+            height: 500,
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _currentDirectory.listSync().length,
+                    itemBuilder: (BuildContext context, int index) {
+                      FileSystemEntity entity = _currentDirectory.listSync()[index];
+                      return ListTile(
+                        leading: entity is Directory
+                            ? Icon(Icons.folder)
+                            : Icon(Icons.insert_drive_file),
+                        title: Text(entity.path.split('/').last),
+                        onTap: () async {
+                          if (entity is Directory) {
+                            await _selectDirectory(entity);
+                          } else {
+                            await _toggleDirectorySelection(_currentDirectory);
+                          }
+                        },
+                        selected: _selectedDirectories.contains(entity),
+                      );
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_upward),
+                      onPressed: _currentDirectory.path == _rootDirectory.path
+                          ? null
+                          : _navigateUp,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(_selectedDirectories);
+                      },
+                      child: Text('Select'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    await SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return _selectedDirectories;
+
